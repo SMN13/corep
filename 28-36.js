43c439a3430b3ec7287f20b1aa65c7eee52bbbd5
sakshin <55727705+SMN13@users.noreply.github.com>
2023-03-04 11:19:57 +0530

    Update and rename 28-35.js to 28-36.js

--- 28-36.js
+++ 28-36.js
@@ -82,43 +82,48 @@ looker.plugins.visualizations.add({
     this._container = element.appendChild(div);
 
   },
-  
+ 
   addDownloadButtonListener: function () {
     const downloadButton = this._container.appendChild(document.createElement('button'));
     downloadButton.innerHTML = 'Download as Excel';
     downloadButton.className = 'download-button';
-    downloadButton.addEventListener('click', (event) => {
-     var uri = 'data:application/vnd.ms-excel;base64,',
-      template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
-      base64 = function(s) {
-        return window.btoa(unescape(encodeURIComponent(s)))
-      },
-      format = function(s, c) {
-        return s.replace(/{(\w+)}/g, function(m, p) {
-          return c[p];
-        })
-      }
-      
-      var table = document.querySelector('table'); 
-      var rows = table.rows;
-      for (var i = 0; i < rows.length; i++) {
-        var cells = rows[i].cells;
-        for (var j = 0; j < cells.length; j++) {
-          var cell = cells[j];
-          var backgroundColor = window.getComputedStyle(cell).backgroundColor;
-          var fontWeight = window.getComputedStyle(cell).fontWeight;
-          var fontFamily = window.getComputedStyle(cell).fontFamily;
-          var fontSize = window.getComputedStyle(cell).fontSize;
-          var style = 'background-color:' + backgroundColor + ';' +
-            'border: 1px solid black;' +
-            'font-weight:' + fontWeight + ';' +
-            'font-size: 11pt;' +
-            'font-family:' + fontFamily + ';' +
-            'mso-number-format: "\ \@";' ;
+    downloadButton.addEventListener('click', async (event) => {
+  
+      const uri = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,';
+      const template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{Worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>';
+      const base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) };
+      const format = function (s, c) {
+        const regex = /style="([^"]*)"/g;
+        return s.replace(/{(\w+)}/g, function (m, p) {
+          const cellHtml = c[p];
+          const cellHtmlWithStyle = cellHtml.replace(regex, function (m, p1) {
+            return 'style="' + p1 + '"';
+          });
+          return cellHtmlWithStyle;
+        });
+      };
+  
+      // Create a new style element and set the default styles
+      const style = document.createElement('style');
+      style.type = 'text/css';
+      style.innerHTML = 'td, th { background-color: white; border: 1px solid black; font-weight: normal; font-size: 11pt; font-family: Calibri, sans-serif; mso-number-format: "\\\@"; }';
+      document.head.appendChild(style);
+  
+      const table = document.querySelector('table');
+      const rows = table.rows;
+  
+      // Loop through each cell and set its style
+      for (let i = 0; i < rows.length; i++) {
+        const cells = rows[i].cells;
+        for (let j = 0; j < cells.length; j++) {
+          const cell = cells[j];
           cell.setAttribute('style', style);
         }
       }
-       
+      
+      const XLSX = document.createElement('script');
+      XLSX.src = 'https://cdn.jsdelivr.net/npm/xlsx/dist/xlsx.full.min.js';
+      document.head.appendChild(XLSX);
       var ctx = { Worksheet: '28', table: table.innerHTML }
       var xl = format(template, ctx);
       const downloadUrl = uri + base64(xl);
