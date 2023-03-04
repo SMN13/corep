931f666b7ab089007fd540d10006a9705484f5f9
sakshin <55727705+SMN13@users.noreply.github.com>
2023-03-04 10:41:26 +0530

    Update and rename 28-29.js to 28-30.js

--- 28-30.js
+++ 28-30.js
@@ -84,6 +84,10 @@ looker.plugins.visualizations.add({
   },
   
   exceller: function () { 
+    const downloadButton = this._container.appendChild(document.createElement('button'));
+    downloadButton.innerHTML = 'Download as Excel';
+    downloadButton.className = 'download-button';
+    downloadButton.addEventListener('click', (event) => {
         var uri = 'data:application/vnd.ms-excel;base64,',
           template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
           base64 = function(s) {
@@ -103,6 +107,7 @@ looker.plugins.visualizations.add({
         link.download = "export.xls";
         link.href = uri + base64(format(template, ctx))
         link.click(); 
+    });
   },
   
 
