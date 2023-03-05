d71365c43870d807bacc8f840fc10633893c4446
sakshin <55727705+SMN13@users.noreply.github.com>
2023-03-05 11:17:03 +0530

    Update and rename 28-106.js to 28-107.js

--- 28-107.js
+++ 28-107.js
@@ -88,7 +88,7 @@ looker.plugins.visualizations.add({
     downloadButton.innerHTML = 'Download as Excel';
     downloadButton.className = 'download-button';
     downloadButton.addEventListener('click', (event) => {
-      var uri = 'data:application/vnd.ms-excel;base64,'
+      var uri = 'data:application/octet-stream.ms-excel;base64,'
         , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{Worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>'
         , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
         , format = function (s, c) {
